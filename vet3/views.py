9248ea4554075efd5bc8c3f411d0c0bfb5437c3c
Effly <effly88@gmail.com>
2025-01-09 21:39:49 +0300

    add 10 min delay

--- vet3/views.py
+++ vet3/views.py
@@ -3,6 +3,7 @@ from .forms import AppointmentForm
 from django.shortcuts import render, redirect
 from datetime import timedelta, datetime
 from .models import Appointment
+import pprint
 
 
 def home(request):
@@ -18,17 +19,19 @@ def appointment_view(request):
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
         if form.is_valid():
+            pp = pprint.PrettyPrinter(indent=4)
+
             service = form.cleaned_data['service']
             date = form.cleaned_data['date']
             time = form.cleaned_data['time']
 
             # Получаем последнюю запись с тем же service
             last_appointment = Appointment.objects.filter(service=service).order_by('-date', '-time').first()
-
+            pp.pprint(last_appointment)
             if last_appointment:
                 last_datetime = datetime.combine(last_appointment.date, last_appointment.time)
                 new_datetime = datetime.combine(date, time)
-
+                pp.pprint((new_datetime - last_datetime).total_seconds())
                 # Проверяем разницу во времени
                 if abs((new_datetime - last_datetime).total_seconds()) < 600:  # 600 секунд = 10 минут
                     form.add_error(None, 'Разница между временем этой записи и последней записи менее 10 минут. Пожалуйста, выберите другое время.')
